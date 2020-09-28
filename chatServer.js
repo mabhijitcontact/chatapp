5ad4eabcf41dd953663e9c1c9a0bb5489d145e8c
Abhijit Mukherjee <mabhijit.contact@gmail.com>
2020-09-29 02:36:23 +0530

    simple chat app version 1

---
+++ chatServer.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+
+let usersJoined = [];
+let userInfo = {};
+// io.on = io.of('/').on I have made entire things on root namespace
+
+io.on('connection', (socket) => {
+    let userName = socket.handshake.query.username;
+    console.log(socket.client.conn.server.clientsCount + " ---- users connected socket id is : " + socket.id);
+    if (socket.client.conn.server.clientsCount > 2) {
+        io.sockets.connected[socket.id].emit('loginFailed', { text: 'Two Member already joined, sorry try again latter' });
+        io.sockets.connected[socket.id].disconnect();
+    } else {
+        socket.emit('connectionConfirmation', { data: "Welcome to the socketio server" });
+        console.log("Someone connected to the main namespace");
+        userInfo = { socketID: socket.id, username: userName }
+        usersJoined.push(userInfo);
+    }
+
+
+
+    socket.on('newMessageToServer', (msg) => {
+        const fullMsg = {
+            text: msg.text,
+            time: Date.now(),
+            username: userName,
+            avatar: 'https://via.placeholder.com/30'
+        }
+        io.of('/').emit('newMessageToClients', fullMsg);
+    });
+
+    socket.on("leaveChatRequestFromClient", (socketID) => {
+        console.log(socketID);
+        io.sockets.connected[socketID.mySocketID].emit('leaveChatResponseFromClient', { text: "Successfully Disconnected" });
+        io.sockets.connected[socketID.mySocketID].disconnect();
+    });
+});
---
+++ public/scripts.js
@@ -0,0 +1,120 @@
+document.querySelector('#user-loginForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.querySelector('#username').value;
+    if (!checkempty(username)) {
+        alert("Please enter username to chat");
+    } else {
+        connectToServer(username);
+    }
+
+})
+function checkempty(username) {
+    if (username == null ||
+        username == undefined ||
+        username.length == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+function toggleBlock(isTrue) {
+    if (isTrue) {
+        document.getElementById("loginWindow").classList.remove('showBlock');
+        document.getElementById("loginWindow").classList.add("hideBlock")
+        document.querySelector('#chatWindow').classList.remove('hideBlock');
+        document.querySelector('#chatWindow').classList.add('showBlock');
+    } else {
+        document.getElementById("loginWindow").classList.remove('hideBlock');
+        document.getElementById("loginWindow").classList.add("showBlock")
+        document.querySelector('#chatWindow').classList.remove('showBlock');
+        document.querySelector('#chatWindow').classList.add('hideBlock');
+    }
+
+}
+
+function connectToServer(usr) {
+    let mySocketID;
+    const socket = io('http://localhost:9000', {
+        query: {
+            username: usr
+        }
+    }); // the / namespace/endpoint
+
+    socket.on('connect', () => {
+        mySocketID = socket.id;
+        console.log('My Socket ID is: =>' + socket.id)
+    });
+
+    socket.on('loginFailed', (msg) => {
+        console.log(msg)
+        document.getElementById("loginAlert").classList.remove('hideBlock');
+        document.getElementById("loginAlert").innerHTML = msg.text;
+    });
+
+    socket.on('connectionConfirmation', (msg) => {
+        toggleBlock(true);
+        document.getElementById("loginAlert").classList.add('hideBlock');
+        document.getElementById("loginAlert").innerHTML = "";
+        console.log(msg)
+    })
+
+    socket.on('newMessageToClients', (msg) => {
+        const newMsg = buildHTML(msg);
+        document.querySelector('#messages').innerHTML += newMsg
+    });
+
+    socket.on('leaveChatResponseFromClient', (msg) => {
+        console.log(msg)
+        document.getElementById("loginAlert").classList.remove('hideBlock');
+        document.getElementById("loginAlert").innerHTML = msg.text;
+    });
+
+    document.querySelector('#user-inputForm').addEventListener('submit', (event) => {
+        event.preventDefault();
+        const newMessage = document.querySelector('#user-message').value;
+        if (newMessage.charAt(0) == "/") {
+            cmdStr = newMessage.substring(1, newMessage.length).toUpperCase();
+            switch (cmdStr) {
+                case 'END':
+                    toggleBlock(false);
+                    mySocketID
+                    socket.emit('leaveChatRequestFromClient', { mySocketID });
+                    break;
+                case 'FAQ':
+                    break;
+                case 'default':
+                    socket.emit('newMessageToServer', { text: newMessage });
+                    break;
+            }
+        } else {
+            socket.emit('newMessageToServer', { text: newMessage });
+        }
+    });
+}
+
+
+
+function buildHTML(msg) {
+    const convertedDate = new Date(msg.time).toLocaleString();
+    const newHTML = `
+    <li>
+        <div class="user-image">
+            <img src="${msg.avatar}" />
+        </div>
+        <div class="user-message">
+            <div class="user-name-time">${msg.username} <span>${convertedDate}</span></div>
+            <div class="message-text">${msg.text}</div>
+        </div>
+    </li>    
+    `
+    return newHTML;
+}
+// socket.on('ping',()=>{
+//     console.log('Ping was recieved from the server.');
+//     console.log(io.protocol)
+// })
+
+// socket.on('pong',(latency)=>{
+//     console.log(latency);
+//     console.log("Pong was sent to the server.")
+// })
